e5c82e3421da5dff453942d607eea7e77b437d85
Jhosefer Senna <jhosefer_nf@hotmail.com>
2021-01-07 01:24:12 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Projeto X - Login</title>
+  <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+
+<body>
+  <div class="login">
+    <h1>Projeto X</h1>
+    <form id="frm-login">
+      <input type="text" name="nome" id="nome" placeholder="Nome" />
+      <input type="text" name="email" id="email" placeholder="email" />
+      <input type="password" name="senha" id="senha" placeholder="Senha" />
+      <button type="button" class="btn btn-primary btn-block btn-large" id="btn-login">Login</button>
+    </form>
+  </div>
+
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script>
+  $(document).ready(function() {
+
+  });
+</script>
+
+</html>
